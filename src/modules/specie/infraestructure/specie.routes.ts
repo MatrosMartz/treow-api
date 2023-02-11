78f4dc52f6792a24129f5aaed4e9ab8f052fa5d8
TsonkoAlt <matrosmartz@gmail.com>
2023-02-10 20:17:36 -0600

    refactor(specie): change route.route in favor to route.<method> and crtl to ctrls

--- src/modules/specie/infraestructure/specie.routes.ts
+++ src/modules/specie/infraestructure/specie.routes.ts
@@ -3,20 +3,15 @@ import { FastifyInstance } from 'fastify'
 import { SpecieUseCase } from '$specie/application'
 
 import { SpecieCtrl } from './specie.ctrl'
-import { routesSchemas } from './route-schemas'
+import { schemas } from './route-schemas'
 import Repo from './repositorys'
 
 const useCase = new SpecieUseCase(new Repo())
 
-const ctrl = new SpecieCtrl(useCase)
+const ctrls = new SpecieCtrl(useCase)
 
 async function speciesRouter(route: FastifyInstance): Promise<void> {
-	route.route({
-		method: 'GET',
-		url: '/',
-		schema: routesSchemas.list,
-		handler: ctrl.list,
-	})
+	route.get('/', { schema: schemas.list }, ctrls.list)
 }
 
 export default speciesRouter
